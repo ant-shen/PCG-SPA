1eb79a5ebf80f7c6ed853d9ad6151c5b4e963ef6
ant-shen <104231150+ant-shen@users.noreply.github.com>
2024-08-23 20:31:13 -0700

    Update landHolding.js

--- backend/models/landHolding.js
+++ backend/models/landHolding.js
@@ -1,16 +1,48 @@
 const mongoose = require('mongoose');
 
-const landHoldingSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  legalEntity: { type: String },
-  netMineralAcres: { type: Number },
-  mineralOwnerRoyalty: { type: Number },
-  sectionName: { type: String },
-  section: { type: String },
-  township: { type: String },
-  range: { type: String },
-  titleSource: { type: String, enum: ['Class A', 'Class B', 'Class C', 'Class D'] },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'Owner', required: true }
+const LandHoldingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Owner',
+    required: true,
+  },
+  legalEntity: {
+    type: String,
+    required: true,
+  },
+  netMineralAcres: {
+    type: Number,
+    required: true,
+  },
+  mineralOwnerRoyalty: {
+    type: Number,
+    required: true,
+  },
+  sectionName: {
+    type: String,
+    required: true,
+  },
+  section: {
+    type: String,
+    required: true,
+  },
+  township: {
+    type: String,
+    required: true,
+  },
+  range: {
+    type: String,
+    required: true,
+  },
+  titleSource: {
+    type: String,
+    enum: ['Class A', 'Class B', 'Class C', 'Class D'],
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('LandHolding', landHoldingSchema);
+module.exports = mongoose.model('LandHolding', LandHoldingSchema);
